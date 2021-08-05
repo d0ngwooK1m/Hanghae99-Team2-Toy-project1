b27520121f073a1fc359a64eb1eb0c7a288f7592
CH_Hwang <hch950627@naver.com>
2021-08-05 19:16:44 +0900

    list

---
+++ script.js
@@ -0,0 +1,38 @@
+const Option_Jjim = document.querySelector('#Option_Jjim')
+const Jjim_star = document.querySelector('#star')
+const Option_Like = document.querySelector('#Option_Like')
+const Like_heart = document.querySelector('#heart')
+
+let Jjim_count =false;
+function Jjim_starControl(){
+    Jjim_count = !Jjim_count;
+}
+
+let Like_count =false;
+function Like_heartControl(){
+    Like_count = !Like_count;
+}
+
+function Jjim(){
+    if(Jjim_count){
+        Jjim_star.src = "img/yellow-star.png";
+    }else{
+        Jjim_star.src = 'img/star.png';
+    }
+}
+
+function Like(){
+    if(Like_count){
+        Like_heart.src = "img/red-heart.png";
+    }else{
+        Like_heart.src = "img/heart.png";
+    }
+}
+
+Option_Jjim.addEventListener('click', Jjim)
+Jjim_star.addEventListener('click', Jjim)
+Option_Jjim.addEventListener('click', Jjim_starControl)
+
+Option_Like.addEventListener('click', Like)
+Like_heart.addEventListener('click',Like)
+Option_Like.addEventListener('click', Like_heartControl)
