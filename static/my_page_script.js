8a45c4fba1769af2d4000df73799be5cb05f6a3f
dam-lee <studymida88@gmail.com>
2021-08-22 16:34:27 +0900

    myPage에서 로그인한 회원의 글만 보여지도록 분리

--- static/my_page_script.js
+++ static/my_page_script.js
@@ -1,6 +1,5 @@
 function myPageList() {
-  console.log("list");
-  fetch("/view", {
+  fetch("/myPage/list", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
@@ -8,7 +7,51 @@ function myPageList() {
   })
     .then((response) => response.json())
     .then((response) => {
-      console.log("mypage response ", response);
+      const myList = response["my_list"];
+      console.log("mypage response ", myList);
+      for (let i = 0; i < myList.length; i++) {
+        const url = myList[i]["url"];
+        const title = myList[i]["title"];
+        const likes = myList[i]["likes"];
+        const imgsrc = myList[i]["imgsrc"];
+        const id = myList[i]["id"];
+        const mypage_html = `
+        <div class="ListBg">
+                            <div class="ListFlex">
+                                <div class="click-wrap" onclick="showDetail('${id}')">
+                                    <div class="imgHidden-box">
+                                        <img src="${imgsrc}" class="classImg">
+                                        <button class="Option_Jjim">
+                                            <img src="../static/img/heart.svg" class="heart">
+                                        </button>
+                                    </div>  
+                                    <h3 class ="title">${title}</h3>
+                                    <div class ="numCount">
+                                        <div class = "likeNum">
+                                            <img src="../static/img/likeUp3.png">
+                                            <span>${likes}</span>
+                                        </div>
+                                        <div class = "JjimNum">
+                                            <img src="../static/img/Rheart.svg">
+                                            <span>0</span>
+                                        </div>
+                                    </div>
+                                    <hr/>
+                                </div>
+                                <div class="Option">
+                                    <div class="Like Option_Like" onclick="updateLike('${id}')">
+                                        <img src="../static/img/thumbsup.svg" class="thumbsUp">
+                                        <span class="likes-btn">추천하기</span>
+                                    </div>
+                                    <a class="Link" href="${url}" target= '_blank'>
+                                        <span>바로가기</span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+        `;
+        $(".mypage_cardList").append(mypage_html);
+      }
     });
 }
 
