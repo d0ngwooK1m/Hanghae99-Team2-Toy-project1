b2c43e7b99ab81555b3f9ce57774f6614322b0db
dam-lee <studymida88@gmail.com>
2021-08-25 20:36:31 +0900

    글등록 예외처리 추가

--- static/js/base.js
+++ static/js/base.js
@@ -218,23 +218,29 @@ function posting() {
   let url = document.getElementById("url").value;
   let title = document.getElementById("title").value;
   let desc = document.getElementById("description").value;
-  $.ajax({
-    type: "POST",
-    url: "/test",
-    data: {
-      url_give: url,
-      title_give: title,
-      desc_give: desc,
-      imgsrc_give: imgsrc,
-    },
-    success: function (response) {
-      alert(response["msg"]);
-      window.location.reload();
-    },
-    error: function (e) {
-      alert("url 주소를 다시 확인해주세요");
-    },
-  });
+  if (title === "") {
+    alert("제목을 입력해주세요.");
+  } else if (desc === "") {
+    alert("내용을 입력해주세요");
+  } else {
+    $.ajax({
+      type: "POST",
+      url: "/test",
+      data: {
+        url_give: url,
+        title_give: title,
+        desc_give: desc,
+        imgsrc_give: imgsrc,
+      },
+      success: function (response) {
+        alert(response["msg"]);
+        window.location.reload();
+      },
+      error: function (e) {
+        alert("url 주소를 다시 확인해주세요");
+      },
+    });
+  }
 }
 if (createBtn !== null) {
   createBtn.addEventListener("click", posting);
